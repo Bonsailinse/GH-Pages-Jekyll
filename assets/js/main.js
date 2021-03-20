6b812be9b9d934e75052eb12be152589940aa3b9
Bonsailinse <mail@bonsailinse.net>
2021-03-20 22:51:51 +0100

    Refactor: Identity.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,13 +29,3 @@
       : area.classList.remove("blurry");
   });
 })();
-
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  };
