c22c91b305c16f9d1d190b97f0caccff6f35e800
Bonsailinse <mail@bonsailinse.net>
2021-03-20 19:56:38 +0100

    Merge Identity JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,13 @@
       : area.classList.remove("blurry");
   });
 })();
+
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  };
